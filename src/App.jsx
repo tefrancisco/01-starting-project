4b741d06561abd6f6c5fe158f74168f5950ffbec
tefrancisco <dzfranciscodz@gmail.com>
2025-12-02 15:19:55 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { CORE_CONCEPTS } from './data.js'
+import Header from './components/Header/Header.jsx'
+import CoreConcept from './components/CoreConcept.jsx'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <ul>
+            <CoreConcept 
+            {...CORE_CONCEPTS[0]}
+            />
+            <CoreConcept 
+            {...CORE_CONCEPTS[1]}
+            />
+            <CoreConcept 
+            {...CORE_CONCEPTS[2]}
+            />
+            <CoreConcept 
+            title={CORE_CONCEPTS[3].title}
+            description={CORE_CONCEPTS[3].description}
+            image={CORE_CONCEPTS[3].image}
+            />
+          </ul>
+        </section>
+        <h2>Time to get started!</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
